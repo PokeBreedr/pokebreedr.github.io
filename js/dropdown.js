994776f717b27f607f230717724df0017f6dc28c
AleixLinares <aleixlinaresbaldrich@gmail.com>
2026-07-05 11:02:35 +0200

    first deployment

---
+++ js/dropdown.js
@@ -0,0 +1,36 @@
+﻿window.dropdownInterop = {
+
+    listeners: {},
+
+    registerOutsideClick: function (id, element, dotnetHelper) {
+
+        const handler = function (event) {
+
+            if (!element || !element.contains) {
+                return;
+            }
+
+            if (!element.contains(event.target)) {
+
+                dotnetHelper.invokeMethodAsync('CloseDropdown')
+                    .catch(() => { });
+            }
+        };
+
+        document.addEventListener('click', handler);
+
+        this.listeners[id] = handler;
+    },
+
+    unregisterOutsideClick: function (id) {
+
+        const handler = this.listeners[id];
+
+        if (handler) {
+
+            document.removeEventListener('click', handler);
+
+            delete this.listeners[id];
+        }
+    }
+};
